f54ed66b7ae9888321a5fcd4b408db4bbb3e0c72
yuwnagcode <m15000825205@hotmail.com>
2016-07-31 12:16:23 +0800

    Commit 2016/07/31 12:16 插入排序

---
+++ src/cn/org/yuwang/sort/InsertionSort.java
@@ -0,0 +1,21 @@
+package cn.org.yuwang.sort;
+
+/**
+ * Created by m1500 on 2016/7/31.
+ */
+public class InsertionSort {
+    public int[] sort(int nums[]){
+        int key;
+        int j;
+        for(int i=1;i<nums.length;i++){
+            key=nums[i];
+            j=i-1;
+            while (j>=0&&key<nums[j]){
+                nums[j+1]=nums[j];
+                j--;
+            }
+            nums[j+1]=key;
+        }
+        return nums;
+    }
+}
