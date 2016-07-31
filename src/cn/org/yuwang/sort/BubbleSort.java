665df307dccb9760077c4a4f5505930cdd99d5d6
yuwnagcode <m15000825205@hotmail.com>
2016-07-31 14:56:57 +0800

    Commit 2016/07/31 14:56 冒泡排序

--- src/cn/org/yuwang/sort/BubbleSort.java
+++ src/cn/org/yuwang/sort/BubbleSort.java
@@ -5,6 +5,16 @@ package cn.org.yuwang.sort;
  */
 public class BubbleSort {
     public int [] sort(int [] nums){
-        for(int )
+        int temp;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+        return nums;
     }
 }
