4759ef42c703e32bc18e016b4f20e4ade5701fcf
yuwang <work@yuwang.org.cn>
2016-08-01 07:59:12 +0800

    Commit 2016/8/1 7:58 从头到尾打印链表

---
+++ src/cn/org/yuwang/JzOffer/从尾到头打印链表.java
@@ -0,0 +1,23 @@
+package cn.org.yuwang.JzOffer;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * Created by wuy on 2016/8/1.
+ */
+public class 从尾到头打印链表 {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> list =new ArrayList<Integer>();
+        Stack<Integer> stack=new Stack<Integer>();
+        while (listNode!=null){
+            stack.push(listNode.val);
+            listNode=listNode.next;
+        }
+        int n=stack.size();
+        for(int i=0;i<n;i++){
+            list.add(stack.pop());
+        }
+        return list;
+    }
+}
