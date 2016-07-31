e85e61967c0c7c9cdc8304857ab9856cc0bc925f
yuwnagcode <m15000825205@hotmail.com>
2016-07-31 15:12:13 +0800

    Commit 2016/07/31 15:11 插入排序

---
+++ src/cn/org/yuwang/sort/SelectionSort.java
@@ -0,0 +1,25 @@
+package cn.org.yuwang.sort;
+
+/**
+ * Created by m1500 on 2016/7/31.
+ */
+public class SelectionSort {
+    int minN;
+    int temp;
+    public int [] sort(int nums[]){
+        for(int i=0;i<nums.length;i++){
+            minN=i;
+            for(int j=i+1;j<nums.length;j++){
+                if (nums[j]<nums[minN]){
+                    minN=j;
+                }
+            }
+            if (minN!=i){
+                temp=nums[i];
+                nums[i]=nums[minN];
+                nums[minN]=temp;
+            }
+        }
+        return nums;
+    }
+}
