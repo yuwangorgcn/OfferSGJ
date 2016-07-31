de9aa15052a3197cf8b3a317fd095aa0c0a788f5
yuwnagcode <m15000825205@hotmail.com>
2016-07-31 10:37:00 +0800

    Commit 2016/07/31 10:36

--- src/cn/org/yuwang/sort/QuitSort.java
+++ src/cn/org/yuwang/sort/QuitSort.java
@@ -4,4 +4,26 @@ package cn.org.yuwang.sort;
  * Created by m1500 on 2016/7/31.
  */
 public class QuitSort {
+    public int [] sort(int[] nums,int left,int right){
+        if(left>=right){
+            return nums;
+        }
+        int low=left;
+        int high=right;
+        int key=nums[left];
+        while (low<high){
+            while (low<high && nums[high]>=key){
+                high--;
+            }
+            nums[low]=nums[high];
+            while (low<high && nums[low]<key){
+                low++;
+            }
+            nums[high]=nums[low];
+        }
+        nums[low]=key;
+        sort(nums,left,low-1);
+        sort(nums,low+1,right);
+        return nums;
+    }
 }
